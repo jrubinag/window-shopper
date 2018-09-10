c8daf8ff67f1ff52737a5eb0118a9d25c3b95bd8
Jonathan Rubina <jonathanrubina@Jonathans-MacBook-Pro.local>
2018-09-10 00:15:12 -0500

    View, input accesory views & customButton IBDesignable

---
+++ window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Jonathan Rubina on 9/9/18.
+//  Copyright © 2018 Jonathan Rubina. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTextField: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+        
+    }
+    
+    func customizeView(){
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.26)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)] )
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
