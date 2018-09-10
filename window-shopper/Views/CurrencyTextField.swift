fe382de286893fa5e7268d6e18f8309ed9fdb9b4
Jonathan Rubina <jonathanrubina@Jonathans-MacBook-Pro.local>
2018-09-10 01:23:38 -0500

    Custom drawing currency symbol

--- window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/Views/CurrencyTextField.swift
@@ -11,6 +11,20 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size : CGFloat = 20
+        let currencyLbl = UILabel(frame : CGRect(x: 5, y: (frame.size.height / 2) - size/2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.4845534581, green: 0.4845534581, blue: 0.4845534581, alpha: 0.6798480308)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -24,6 +38,7 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.26)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)] )
